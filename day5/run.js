7c6da6e2ed6dddd37ae6ddfc29191b80eb8e0efd
Mindaugas Paškevičius <mindaugas.paskevicius@metasite.net>
2018-12-30 02:47:47 +0200

    Change Formatting

--- day5/run.js
+++ day5/run.js
@@ -1,15 +1,13 @@
-
-
 const lowerCharCodeMap = Array(26).fill(97).reduce((p, c, i) => ({ ...p, [String.fromCharCode(c + i)]: String.fromCharCode(c + i - 32) }), {});
 const upperCharCodeMap = Array(26).fill(97).reduce((p, c, i) => ({ ...p, [String.fromCharCode(c + i - 32)]: String.fromCharCode(c + i) }), {});
-const charMap = { ...lowerCharCodeMap, ...upperCharCodeMap }; // ?
+const charMap = { ...lowerCharCodeMap, ...upperCharCodeMap };
 
 function reduce(pointer, sequence) {
   const reductable = sequence[pointer] === charMap[sequence[pointer + 1]];
   if (reductable) {
     const part1 = sequence.slice(0, pointer);
     const part2 = sequence.slice(pointer + 2);
-    return [pointer - 1, part1 + part2]; // ?
+    return [pointer - 1, part1 + part2];
   }
   return [pointer + 1, sequence];
 }
