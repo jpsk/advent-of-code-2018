710403b5c26c5c5ca1f85f696670de945fec6c98
Mindaugas Paškevičius <mindaugas.paskevicius@metasite.net>
2018-12-30 02:47:01 +0200

    Change Formatting, remove debug values

--- day5/run.js
+++ day5/run.js
@@ -1,30 +1,28 @@
 
 
-const lowerCharCodeMap = Array(26).fill(97).reduce((p, c, i) => ({...p, [String.fromCharCode(c+i)]: String.fromCharCode(c+i - 32)}),{});
-const upperCharCodeMap = Array(26).fill(97).reduce((p, c, i) => ({...p, [String.fromCharCode(c+i - 32)]: String.fromCharCode(c+i)}),{});
-const charMap = {...lowerCharCodeMap, ...upperCharCodeMap}; // ?
+const lowerCharCodeMap = Array(26).fill(97).reduce((p, c, i) => ({ ...p, [String.fromCharCode(c + i)]: String.fromCharCode(c + i - 32) }), {});
+const upperCharCodeMap = Array(26).fill(97).reduce((p, c, i) => ({ ...p, [String.fromCharCode(c + i - 32)]: String.fromCharCode(c + i) }), {});
+const charMap = { ...lowerCharCodeMap, ...upperCharCodeMap }; // ?
 
-function reduce (pointer, sequence) {
-  const reductable = sequence[pointer] === charMap[sequence[pointer+1]];
+function reduce(pointer, sequence) {
+  const reductable = sequence[pointer] === charMap[sequence[pointer + 1]];
   if (reductable) {
     const part1 = sequence.slice(0, pointer);
-    const part2 = sequence.slice(pointer+2);
-    return [pointer-1, part1 + part2]; // ?
+    const part2 = sequence.slice(pointer + 2);
+    return [pointer - 1, part1 + part2]; // ?
   }
-  return [pointer+1, sequence];
+  return [pointer + 1, sequence];
 }
 
 function part1(input) {
   let pointer = 0;
   let sequence = input;
-  let obs;
 
-  while (pointer < sequence.length -1 ) {
+  while (pointer < sequence.length - 1) {
     const [newPointer, newSequence] = reduce(pointer, sequence);
     length = newSequence.length;
     pointer = newPointer;
     sequence = newSequence;
-    obs = newSequence.slice(0, 50);
   }
 
   return sequence.length;
