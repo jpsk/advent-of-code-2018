78bb9fcdc043b1d16b2bacd6256272a8cea1cd42
Mindaugas Paškevičius <mindaugas.paskevicius@metasite.net>
2018-12-30 02:42:37 +0200

    add day 5

--- day5/run.js
+++ day5/run.js
@@ -1,9 +1,41 @@
+
+
+const lowerCharCodeMap = Array(26).fill(97).reduce((p, c, i) => ({...p, [String.fromCharCode(c+i)]: String.fromCharCode(c+i - 32)}),{});
+const upperCharCodeMap = Array(26).fill(97).reduce((p, c, i) => ({...p, [String.fromCharCode(c+i - 32)]: String.fromCharCode(c+i)}),{});
+const charMap = {...lowerCharCodeMap, ...upperCharCodeMap}; // ?
+
+function reduce (pointer, sequence) {
+  const reductable = sequence[pointer] === charMap[sequence[pointer+1]];
+  if (reductable) {
+    const part1 = sequence.slice(0, pointer);
+    const part2 = sequence.slice(pointer+2);
+    return [pointer-1, part1 + part2]; // ?
+  }
+  return [pointer+1, sequence];
+}
+
 function part1(input) {
-  return 10;
+  let pointer = 0;
+  let sequence = input;
+  let obs;
+
+  while (pointer < sequence.length -1 ) {
+    const [newPointer, newSequence] = reduce(pointer, sequence);
+    length = newSequence.length;
+    pointer = newPointer;
+    sequence = newSequence;
+    obs = newSequence.slice(0, 50);
+  }
+
+  return sequence.length;
 }
 
-function part2() {
-  return 'part2';
+function part2(input) {
+  const lengthMap = Object.keys(lowerCharCodeMap).map(key => {
+    const trimmed = input.replace(new RegExp(`${key}|${lowerCharCodeMap[key]}`, 'g'), '');
+    return part1(trimmed);
+  })
+  return Math.min(...lengthMap);
 }
 
-module.exports = {part1, part2}
+module.exports = { part1, part2, reduce }
--- day5/run.test.js
+++ day5/run.test.js
@@ -2,3 +2,16 @@ const run = require('./run');
 test('Part1 Test1', () => {
   expect(run.part1('dabAcCaCBAcCcaDA')).toEqual(10);
 })
+
+test('Part2, Test1', () => {
+  expect(run.part2('dabAcCaCBAcCcaDA')).toEqual(4);
+})
+
+test('Test Reduce', () => {
+  expect(run.reduce(4, 'dabAcCaCBAcCcaDA')).toEqual([3,'dabAaCBAcCcaDA']);
+  expect(run.reduce(3, 'dabAaCBAcCcaDA')).toEqual([2,'dabCBAcCcaDA']);
+  expect(run.reduce(2,'dabCBAcCcaDA')).toEqual([3,'dabCBAcCcaDA']);
+  expect(run.reduce(6,'dabCBAcCcaDA')).toEqual([5,'dabCBAcaDA']);
+})
+
+
